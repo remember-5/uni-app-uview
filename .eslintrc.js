8f5c6d76842d46e5a2e61336ce4497a39deb21cf
wangjiahao <1332661444@qq.com>
2022-09-19 18:55:00 +0800

    升级uni

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
+  },
+  // 配置js全局变量，因为是uni-app，全局的uni是不需要引入的，还有5+的plus对象
+  globals: {
+    uni: 'readonly',
+    plus: 'readonly',
+    wx: 'readonly'
   },
   extends: [
     "plugin:vue/essential",
