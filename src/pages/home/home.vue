d5bd6425a9e38bc42cc58caa40e7f528a13c050e
wangjiahao <1332661444@qq.com>
2022-09-19 19:01:51 +0800

    升级uni

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -9,7 +9,7 @@ export default {
     return {};
   },
   created() {
-    console.log('home create', this.vuex_user, this.vuex_wang);
+    console.log( 'home create', this.vuex_user, this.vuex_wang);
     this.$u.vuex('vuex_user', '2');
     this.$u.vuex('vuex_wang', '2');
     console.log('home set', this.vuex_user, this.vuex_wang);
