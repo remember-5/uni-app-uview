627ff040b483b44db2f1e303116f27979571ad3d
wangjiahao <1332661444@qq.com>
2023-05-10 17:40:01 +0800

    完善webview后退

--- src/pages/webview/webview.vue
+++ src/pages/webview/webview.vue
@@ -11,7 +11,8 @@
       return {
         url: '',
         backUrl: '',
-        type: ''
+        backType: '',
+        pageNum: 0
       }
     },
     onLoad() {
@@ -20,9 +21,9 @@
       this.backUrl = this.$Route.query.backUrl
       this.backType = this.$Route.query.backType
 
-      this.initPageNum = 0
+      this.pageNum = 0
       // #ifdef H5
-      this.initPageNum = window.history.length
+      this.pageNum = window.history.length
       // #endif
     },
     methods: {
@@ -31,7 +32,6 @@
         // router.replace() 等同于 uni.redirectTo() 跟 router.push 很像，唯一的不同就是，它不会向 history 添加新记录，而是跟它的方法名一样 —— 替换掉当前的 history 记录。
         // router.replaceAll() 等同于 uni.reLaunch() 跟 router.replace 很像，唯一的不同就是，它不会向 history 添加新记录，而是将所有的页面都关掉，打开一个新的页面。
         // router.pushTab() 等同于 uni.switchTab() 跟 router.push 很像，打开 uni-app 自带的tab 菜单
-        console.log(this.backUrl, this.backType)
         if (this.backUrl) {
           if (this.backType === 'switchTab') {
             this.$Router.pushTab({ path: this.backUrl, query: { name: 'wang' } })
@@ -44,7 +44,7 @@
           // currentPageNum = window.history.length
           // // #endif
           // uni.navigateBack({
-          //   delta: currentPageNum - this.initPageNum + 1
+          //   delta: currentPageNum - this.pageNum + 1
           // })
         }
       }
